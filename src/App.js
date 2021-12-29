60cddab6a6fcf1a7c8ff8277e4f6ee5aab2a4cff
Kimihito Tanaka <kimi@kmd.keio.ac.jp>
2021-12-30 01:21:23 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = ({initialCount}) => {
+  let [count, setCount] = useState(initialCount);
+  let [posts, setPosts] = useState([
+    {
+      name: 'Super awesome hooks',
+      body:'Everything is awesome when you are part of a team'
+    }
+  ]);
+  const addOne = () => {
+    setCount(count +1)
+  }
+  const restOne = () => {
+    setCount(prevCount=>(prevCount-1))
+  }
+  const addOnePost = () => {
+    let newpost = {
+      name: 'Super awesome hooks2',
+      body:'Everything is awesome when you are part of a team2'
+    }
+    setPosts([...posts,newpost])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h3>Count: { count }</h3>
+      <button onClick={addOne}>add one</button>
+      <button onClick={restOne}>rest one</button>
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+      
+      {
+        posts.map((item, i)=>(
+        <div key={i}>
+          <hr />
+          <div>name: {item.name}</div>
+          <div>body: {item.body}</div>
+        </div>
+      ))
+      }
+      <button onClick={addOnePost}>add one more post</button>
+    </>
+  )
 }
 
 export default App;
